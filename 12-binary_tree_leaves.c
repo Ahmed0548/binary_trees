a6175890da848f97ce69bb2592edf7356208ea77
Ahmed0548 <ahmedalkasape@gmail.com>
2023-03-30 12:08:30 -0700

    Task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 /**
- * binary_tree_size - function that return the size of a tree
+ * binary_tree_leaves - function that returns the number of leaves in a tree
  * @tree: tree to check
- * Return: size of tree
+ * Return: number of leaves
  */
-size_t binary_tree_size(const binary_tree_t *tree)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t size = 0, r = 0, l = 0;
+	size_t leaf = 0, l = 0, r = 0;
 
 	if (tree == NULL)
 	{
@@ -14,9 +14,9 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	}
 	else
 	{
-		l = binary_tree_size(tree->left);
-		r = binary_tree_size(tree->right);
-		size = r + l + 1;
+		l = binary_tree_leaves(tree->left);
+		r = binary_tree_leaves(tree->right);
+		leaf = l + r;
+		return ((!l && !r) ? leaf + 1 : leaf + 0);
 	}
-	return (size);
 }
